e317b491b7fd5c09e26919d1a44c5cb6fffb6c4e
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-08-19 20:40:07 +0600

    address page is ready and it shows user datas

--- myapp/urls.py
+++ myapp/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path("registration/", views.CustomerRegistrationView.as_view(), name="customerregistration"),
     path("accounts/login/", auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name="login"),
     path("password-reset/", auth_view.PasswordResetView.as_view(template_name="app/password_reset.html", form_class=MyPasswordResetForm), name="password_reset"),
-    #profile URLs
+    #User info URLs
     path('profile/', views.ProfileView.as_view(), name="profile"),
+    path('address/', views.address, name='address'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from urllib import request
 from django.views import View
-from . models import Product
+from . models import Product, Customer
 from django.db.models import Count
 from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
@@ -54,4 +54,23 @@ class ProfileView(View):
         form = CustomerProfileForm()
         return render(request, 'app/profile.html', locals())
     def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            division = form.cleaned_data['division']
+            zipcode = form.cleaned_data['zipcode']
+            reg=Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, division=division, zipcode=zipcode)
+            reg.save()
+            messages.success(request, "Congratulations! Profile is created.")
+        else:
+            messages.warning(request, "Ofs! Invalid Input Data.")
         return render(request, 'app/profile.html', locals())
+    
+#address view
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html', locals())
